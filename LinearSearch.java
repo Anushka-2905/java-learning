80ff897d56f08fd6bbc72a89ead89d5b4e19d886
Anushka-2905 <7972anushkajagtap@gmail.com>
2026-07-05 18:11:29 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -32,3 +32,45 @@ public class LinearSearch {
   Sytem.out.println(result);
   }
 } // output is 2
+
+//---------------------------------------------------------------------------------------------------
+//INSERTION IN ARRAY
+
+/*  "insert into an array" we usually mean placing a new element into an available position by shifting elements*/
+//if we want to insert an element in existing indexx the value at existing may lost
+//ALGORITHM
+/*
+To insert an element at position pos
+1. Start from the last used element.
+2. Move each element one position to the right.
+3. Stop when you reach pos.
+4. Place the new element at pos.
+5.Increase the number of used elements.*/
+
+// JAVA IMPLEMENTATION
+public class Main {
+     public static void main(String[] args) {
+          int[] arr = new int[5];
+
+          arr[0] = 10;
+          arr[1] = 20;
+          arr[2] = 30;
+          arr[3] = 40;
+
+          int size = 4;  // Number of elements currently stored
+          int pos = 2;
+          int value =25;
+          // Shift elements to the right
+          for( int i = size; i > pos; i--) {
+               arr[i] = arr[i-1];
+          }
+          // Insert new value 
+          arr[pos] = value;
+          size++;
+          // Print array
+          for (int i = 0; i<size; i++) {
+               System.out.println(arr[i] + " " );
+          }
+     }
+}
+
